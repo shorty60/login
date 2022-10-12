e557dfcba38b0c222c31c7c0efe118d65e266757
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-10-12 18:08:18 +0800

    feat: initialize project

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { engine } = require('express-handlebars')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('MongoDB connection fail!')
+})
+
+db.once('open', ()=>{
+  console.log('MongoDB connected.')
+})
+
+const app = express()
+const port = 3000
+
+
+app.engine('hbs', engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
